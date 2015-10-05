d40609a1193600b6d1a452f3b5561fbb12047e61
globaltalentgrid <globaltalentgrid@users.noreply.github.com>
2015-10-05 15:56:44 +0530

    Update LaunchMyApp.js

--- www/android/LaunchMyApp.js
+++ www/android/LaunchMyApp.js
@@ -5,30 +5,17 @@
 
   function waitForAndCallHandlerFunction2(itype, url) {
     if (typeof window.handleNewIntent === "function") {
+        window.handleNewIntent(itype, url);
       // Clear the intent when we have a handler
-      cordova.exec(
-          null,
-          null,
-          "LaunchMyApp",
-          "clearIntent",
-          []);
-
-      window.handleNewIntent(itype, url);
+      cordova.exec(null, null, "LaunchMyApp2", "clearIntent", []);
     } else if (remainingAttempts-- > 0) {
       setTimeout(function(){waitForAndCallHandlerFunction2(itype ,url);}, 500);
     }
   }
 
   function triggerShareOpenURL() {
-    cordova.exec(
-        waitForAndCallHandlerFunction2,
-        null,
-        "LaunchMyApp",
-        "checkIntent",
-        []);
+    cordova.exec(waitForAndCallHandlerFunction2, null, "LaunchMyApp2", "checkIntent", []);
   }
-
-
- 
+  
   document.addEventListener("deviceready", triggerShareOpenURL, false);
 }());
