b2377516319c207dd505f326bd04ca853f6a99e7
globaltalentgrid <globaltalentgrid@users.noreply.github.com>
2015-10-06 09:08:09 +0530

    Update LaunchMyApp.java

--- src/android/nl/xservices/plugins/LaunchMyApp.java
+++ src/android/nl/xservices/plugins/LaunchMyApp.java
@@ -38,8 +38,46 @@ public class LaunchMyApp extends CordovaPlugin {
     } else if (ACTION_CHECKINTENT.equalsIgnoreCase(action)) {
       final Intent intent = ((CordovaActivity) this.webView.getContext()).getIntent();
       final String intentString = intent.getDataString();
-      if (intentString != null && intent.getScheme() != null) {
-        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, intent.getDataString()));
+      String action = intent.getAction(); String type = intent.getType();
+       JSONObject parameter = new JSONObject();
+       String hasintentdata = "no";
+          if (Intent.ACTION_SEND.equals(action) && type != null) {
+        if ("text/plain".equals(type)) {
+            	String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+		    	if (sharedText != null) {hasintentdata = "yes";
+		    		parameter.put("itype", "sharedtext");parameter.put("val", sharedtext); 	//webView.loadUrl("javascript:handleNewIntent('sharedtext', '" + sharedText + "');");
+		    		}
+        } else if (type.startsWith("image/")) {
+                 Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+    			if (imageUri != null) {hasintentdata = "yes";
+    				parameter.put("itype", "singleimage");parameter.put("val", imageUri); //webView.loadUrl("javascript:handleNewIntent('singleimage', '" + imageUri + "');");
+    				}
+        } else if (type.startsWith("video/")) {
+                Uri videoUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+    			if (videoUri != null) {hasintentdata = "yes";
+    				parameter.put("itype", "videUri");parameter.put("val", videoUri); //webView.loadUrl("javascript:handleNewIntent('singlevideo', '" + videoUri + "');");
+    			}
+        }
+    } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+        if (type.startsWith("image/")) {
+                ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+			    if (imageUris != null) {hasintentdata = "yes";
+			    	parameter.put("itype", "multipleimages");parameter.put("val", imageUris); //webView.loadUrl("javascript:handleNewIntent('multipleimages', '" + imageUris + "');");
+			    	}
+        } else if (type.startsWith("video/")) {hasintentdata = "yes";
+			    ArrayList<Uri> videoUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+			    if (videoUris != null) {
+			    	parameter.put("itype", "multiplevideos");parameter.put("val", videoUris); //webView.loadUrl("javascript:handleNewIntent('multiplevideos', '" + videoUris + "');");
+			    }
+        }
+    } else {
+    	hasintentdata = "no";
+        // Handle other intents, such as being started from the home screen
+        //callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, intent.getDataString()));
+    }
+    
+      if (hasintentdata == "yes") {
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, parameter));
       } else {
         callbackContext.error("App was not started via the launchmyapp URL scheme. Ignoring this errorcallback is the best approach.");
       }
@@ -50,37 +88,6 @@ public class LaunchMyApp extends CordovaPlugin {
     }
   }
 
-      @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        loadUrl(launchUrl);
-        Intent intent = getIntent();
-        String action = intent.getAction();
-    String type = intent.getType();
-
-    if (Intent.ACTION_SEND.equals(action) && type != null) {
-        if ("text/plain".equals(type)) {
-            	String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-		    	if (sharedText != null) {webView.loadUrl("javascript:handleNewIntent('sharedtext', '" + sharedText + "');");}
-        } else if (type.startsWith("image/")) {
-                 Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-    			if (imageUri != null) {webView.loadUrl("javascript:handleNewIntent('singleimage', '" + imageUri + "');");}
-        } else if (type.startsWith("video/")) {
-                Uri videoUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-    			if (videoUri != null) {webView.loadUrl("javascript:handleNewIntent('singlevideo', '" + videoUri + "');");}
-        }
-    } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
-        if (type.startsWith("image/")) {
-                ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-			    if (imageUris != null) {webView.loadUrl("javascript:handleNewIntent('multipleimages', '" + imageUris + "');");}
-        } else if (type.startsWith("video/")) {
-			    ArrayList<Uri> videoUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-			    if (videoUris != null) {webView.loadUrl("javascript:handleNewIntent('multiplevideos', '" + videoUris + "');");}
-        }
-    } else {
-        // Handle other intents, such as being started from the home screen
-    }
-    }
 
 
   @Override
